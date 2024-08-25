ab9280782ce8116cf100c182dea0fb00802f77fd
Pavan K <23pavank@gmail.com>
2024-08-25 12:54:35 +0530

    Default page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import React, { useState } from 'react';
 import InputForm from '../app/components/InputForm';
-import ResponseDisplay from '../app/components/ResponseDisplay';
-import { BFHLResponse, BFHLRequest } from './types';
 
 const Home: React.FC = () => {
-  const [responseData, setResponseData] = useState<BFHLResponse | null>(null);
-  const [selectedOptions, setSelectedOptions] = useState<string[]>(['alphabets', 'numbers', 'highest_lowercase_alphabet']);
-
   return (
     <div>
       <InputForm />
